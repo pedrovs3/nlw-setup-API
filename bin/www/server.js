2d65fd0e0029eb0c978e70c7528c804a9ac65b8f
Pedro <pedrovs3@hotmail.com>
2023-01-21 19:42:18 -0300

    coment host for listen API

--- bin/www/server.js
+++ bin/www/server.js
@@ -4,7 +4,7 @@ const run = async () => {
   try {
     await fastify.listen({
       port: 3333,
-      host: '0.0.0.0',
+      // host: '0.0.0.0',
     }).then(() => console.log('Running'));
   } catch (e) {
     process.exit(1);
