bc99ee4504b76a4aca8cb2ff2695635979191677
Pedro <pedrovs3@hotmail.com>
2023-01-21 20:10:52 -0300

    tests for netlify upload

--- src/v1/Fastify.ts
+++ src/v1/Fastify.ts
@@ -1,32 +1,32 @@
-import Fastify, {FastifyInstance} from 'fastify';
+import Fastify, { FastifyInstance } from 'fastify';
 import cors from '@fastify/cors';
-import {appRoutes} from './routes/routes';
+import { appRoutes } from './routes';
 
 class App {
-	declare app: FastifyInstance;
+  declare app: FastifyInstance;
 
-	constructor() {
-		// Creating the Fastify Instance
-		this.app = Fastify({
-			logger: true,
-		});
-		// Setting global middlewares in this method.
-		this.middlewares();
-		// Setting the api routes.
-		this.routes();
-	}
+  constructor() {
+    // Creating the Fastify Instance
+    this.app = Fastify({
+      logger: true,
+    });
+    // Setting global middlewares in this method.
+    this.middlewares();
+    // Setting the api routes.
+    this.routes();
+  }
 
-	private async middlewares() {
-		// Middleware of CORS
-		await this.app.register(cors, {
-			origin: true,
-		});
-	}
+  private async middlewares() {
+    // Middleware of CORS
+    await this.app.register(cors, {
+      origin: true,
+    });
+  }
 
-	private routes() {
-		// Register routes of API
-		this.app.register(appRoutes, {prefix: '/.netlify/functions/server'});
-	}
+  private routes() {
+    // Register routes of API
+    this.app.register(appRoutes, { prefix: '/.netlify/functions/server' });
+  }
 }
 
 export default new App().app;
